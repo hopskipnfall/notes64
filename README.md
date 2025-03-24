d875ca948d26bc9a40174623e9d06b258ccb7d6a
Jonn <hopskipnfall@users.noreply.github.com>
2025-03-24 22:09:38 +0900

    Update notes

--- README.md
+++ README.md
@@ -3,3 +3,23 @@
 These are my notes on Smash 64. You can read them at [notes.12cb.dev](https://notes.12cb.dev).
 
 I took these notes using [Obsidian](https://obsidian.md/) and published them using [Quartz](https://github.com/jackyzha0/quartz).
+
+## Building
+
+Install dependencies:
+
+```sh
+$ npm i
+```
+
+Build:
+
+```sh
+$ npx quartz build
+```
+
+Build and serve (locally):
+
+```sh
+$ npx quartz build --serve
+```
--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,8 +14,8 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          Notes by{" "}
+          <a href="https://x.com/6kRt62r2zvKp5Rh">nue</a>
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
